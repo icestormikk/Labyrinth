793422c3c7baedf3e422ecd66e1160abbc20d9ca
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-18 23:00:26 +0300

    Division of the enumeration class into two types: for the maze builder and for the exit seeker

---
+++ src/main/kotlin/com/example/domain/BuilderDirection.kt
@@ -0,0 +1,45 @@
+package com.example.domain
+
+import com.example.configuration.DIRECTION_DOWN_CODE
+import com.example.configuration.DIRECTION_LEFT_CODE
+import com.example.configuration.DIRECTION_RIGHT_CODE
+import com.example.configuration.DIRECTION_UP_CODE
+import com.example.configuration.ALLOWED_CELL_TYPES
+
+private const val DISTANCE = 2
+
+enum class BuilderDirection(
+    val code: Int,
+    val vector: Pair<Int, Int>,
+    val predicate: (Array<Array<Cell>>, Cell) -> Boolean
+) {
+    LEFT(
+        DIRECTION_LEFT_CODE,
+        Pair(0, -DISTANCE),
+        { matrix, cell ->
+            cell.column - DISTANCE > 0 && matrix[cell.row][cell.column - DISTANCE].type in ALLOWED_CELL_TYPES
+        }
+    ),
+    UP(
+        DIRECTION_UP_CODE,
+        Pair(-DISTANCE, 0),
+        { matrix, cell ->
+            cell.row - DISTANCE > 0 && matrix[cell.row - DISTANCE][cell.column].type in ALLOWED_CELL_TYPES
+        }
+    ),
+    RIGHT(
+        DIRECTION_RIGHT_CODE,
+        Pair(0, DISTANCE),
+        { matrix, cell ->
+            cell.column + DISTANCE < matrix[0].size - 1 &&
+                    matrix[cell.row][cell.column + DISTANCE].type in ALLOWED_CELL_TYPES
+        }
+    ),
+    DOWN(
+        DIRECTION_DOWN_CODE,
+        Pair(DISTANCE, 0),
+        { matrix, cell ->
+            cell.row + DISTANCE < matrix.size - 1 && matrix[cell.row + DISTANCE][cell.column].type in ALLOWED_CELL_TYPES
+        }
+    )
+}
---
+++ src/main/kotlin/com/example/domain/PathfinderDirection.kt
@@ -0,0 +1,43 @@
+package com.example.domain
+
+import com.example.configuration.DIRECTION_DOWN_CODE
+import com.example.configuration.DIRECTION_LEFT_CODE
+import com.example.configuration.DIRECTION_RIGHT_CODE
+import com.example.configuration.DIRECTION_UP_CODE
+import com.example.configuration.ALLOWED_CELL_TYPES
+
+enum class PathfinderDirection(
+    val code: Int,
+    val vector: Pair<Int, Int>,
+    val predicate: (Array<Array<Cell>>, Cell) -> Boolean
+) {
+    LEFT(
+        DIRECTION_LEFT_CODE,
+        Pair(0, -1),
+        { matrix, cell ->
+            cell.column - 1 > 0 && matrix[cell.row][cell.column - 1].type in ALLOWED_CELL_TYPES
+        }
+    ),
+    UP(
+        DIRECTION_UP_CODE,
+        Pair(-1, 0),
+        { matrix, cell ->
+            cell.row - 1 > 0 && matrix[cell.row - 1][cell.column].type in ALLOWED_CELL_TYPES
+        }
+    ),
+    RIGHT(
+        DIRECTION_RIGHT_CODE,
+        Pair(0, 1),
+        { matrix, cell ->
+            cell.column + 1 < matrix[0].size - 1 &&
+                    matrix[cell.row][cell.column + 1].type  in ALLOWED_CELL_TYPES
+        }
+    ),
+    DOWN(
+        DIRECTION_DOWN_CODE,
+        Pair(1, 0),
+        { matrix, cell ->
+            cell.row + 1 < matrix.size - 1 && matrix[cell.row + 1][cell.column].type in ALLOWED_CELL_TYPES
+        }
+    )
+}
