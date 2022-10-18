2486b4d74d33c5c9f65e07350e035d8dd9a57869
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-18 23:01:15 +0300

    New file with constants

---
+++ src/main/kotlin/com/example/configuration/constants.kt
@@ -0,0 +1,9 @@
+package com.example.configuration
+
+import com.example.domain.CellType
+
+const val DIRECTION_LEFT_CODE = 1
+const val DIRECTION_UP_CODE = 2
+const val DIRECTION_RIGHT_CODE = 3
+const val DIRECTION_DOWN_CODE = 4
+val ALLOWED_CELL_TYPES = arrayOf(CellType.EXIT, CellType.EMPTY)
