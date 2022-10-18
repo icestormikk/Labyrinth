1c6ed6a29facabef27e3826d8ab5561bce1483e1
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-18 23:03:09 +0300

    New types of Cell entities

--- src/main/kotlin/com/example/domain/CellType.kt
+++ src/main/kotlin/com/example/domain/CellType.kt
@@ -1,5 +1,10 @@
 package com.example.domain
 
 enum class CellType(val code: Int) {
-    EMPTY(0), WALL(1), VISITED(2)
+    EMPTY(0),
+    WALL(1),
+    VISITED(2),
+    ENTER(3),
+    EXIT(4),
+    PATH(5)
 }
