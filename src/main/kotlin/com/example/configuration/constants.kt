56aad806cc1dfd6eb525beaf9e73d41fb199b856
icestormikk <jigalow.pavel2015@yandex.ru>
2022-10-19 23:12:30 +0300

    Added docs for lists of constants

--- src/main/kotlin/com/example/configuration/constants.kt
+++ src/main/kotlin/com/example/configuration/constants.kt
@@ -6,4 +6,13 @@ const val DIRECTION_LEFT_CODE = 1
 const val DIRECTION_UP_CODE = 2
 const val DIRECTION_RIGHT_CODE = 3
 const val DIRECTION_DOWN_CODE = 4
+
+/**
+ * Types of cells through which movement is possible
+ */
 val ALLOWED_CELL_TYPES = arrayOf(CellType.EXIT, CellType.EMPTY)
+
+/**
+ * Immutable on [CellType.VISITED] cell types
+ */
+val SERVICE_CELL_TYPES = arrayOf(CellType.ENTER, CellType.EXIT, CellType.WALL)
